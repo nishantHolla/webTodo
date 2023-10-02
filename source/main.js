f07ae73839ab98ed76b059a6c47cd132d4cb86b0
nishantHolla <nishantholla19@gmail.com>
2023-10-02 20:17:28 +0530

    Update Todomanager class
    Following methods are added:
    - addTodo
    - removeTodo
    - getTodo
    - logTodo
    - log
    
    Following methods are updated:
    - validateIndex

--- source/main.js
+++ source/main.js
@@ -5,4 +5,9 @@ import TodoManager from "./scripts/todoManager.js"
 TodoManager.addCollection('collection A')
 TodoManager.addCollection('collection B')
 TodoManager.removeCollection(1)
-TodoManager.logCollection(0)
+
+TodoManager.addTodo('task 1', 'description of task 1', 0)
+TodoManager.addTodo('task 2', 'description of task 2', 0)
+TodoManager.removeTodo(0, 0)
+TodoManager.logTodo(0, 0)
+TodoManager.log()
--- source/scripts/todoManager.js
+++ source/scripts/todoManager.js
@@ -2,18 +2,18 @@
 const TodoManager = new class {
     #dataBase = []
 
-    #validateIndex(index) {
-        if (index < 0 && Math.abs(index) < this.#dataBase.length + 1)
-            return this.#dataBase.length + index
+    #validateIndex(index, length) {
+        if (index < 0 && Math.abs(index) < length + 1)
+            return length + index
 
-        else if (index >=0 && index < this.#dataBase.length)
+        else if (index >=0 && index < length)
             return index
 
         return undefined
     }
 
     addCollection(collectionName, index) {
-        index = this.#validateIndex(index)
+        index = this.#validateIndex(index, this.#dataBase.length)
         if (index === undefined)
             index = this.#dataBase.length
 
@@ -26,7 +26,7 @@ const TodoManager = new class {
     }
 
     removeCollection(index) {
-        index = this.#validateIndex(index)
+        index = this.#validateIndex(index, this.#dataBase.length)
         if (index === undefined)
             return
 
@@ -34,7 +34,7 @@ const TodoManager = new class {
     }
 
     getCollection(index) {
-        index = this.#validateIndex
+        index = this.#validateIndex(index, this.#dataBase.length)
         if (index === undefined)
             return
 
@@ -42,11 +42,64 @@ const TodoManager = new class {
     }
 
     logCollection(index) {
-        index = this.#validateIndex(index)
+        index = this.#validateIndex(index, this.#dataBase.length)
         if (index === undefined)
             return
 
         console.log(this.#dataBase[index])
+    }
+
+    addTodo(title, message, collectionIndex, index) {
+        collectionIndex = this.#validateIndex(collectionIndex, this.#dataBase.length)
+        if (collectionIndex === undefined)
+            return
+
+        const collection = this.getCollection(collectionIndex)
+        index = this.#validateIndex(index, collection.todos.length)
+        if (index === undefined)
+            index = collection.todos.length
+
+        collection.todos.splice(index, 0, {
+            title,
+            message,
+            done: false
+        })
+    }
+
+    removeTodo(collectionIndex, index) {
+        collectionIndex = this.#validateIndex(collectionIndex, this.#dataBase.length)
+        if (collectionIndex === undefined)
+            return
+
+        const collection = this.getCollection(collectionIndex)
+        index = this.#validateIndex(index, collection.todos.length)
+        if (index === undefined)
+            return
+
+        collection.todos.splice(index, 1)
+    }
+
+    getTodo(collectionIndex, index) {
+        collectionIndex = this.#validateIndex(collectionIndex, this.#dataBase.length)
+        if (collectionIndex === undefined)
+            return
+
+        const collection = this.getCollection(collectionIndex)
+        index = this.#validateIndex(index, collection.todos.length)
+        if (index === undefined)
+            return
+
+        return collection.todos[index]
+    }
+
+    logTodo(collectionIndex, index) {
+        const todo = this.getTodo(collectionIndex, index)
+        if (todo)
+            console.log(todo)
+    }
+
+    log() {
+        console.log(this.#dataBase)
     }
 
 }
